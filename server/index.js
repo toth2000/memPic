8bebce0b2e0123f47d005ddf3034169e918fa46d
toth2000 <tothagata.bhattacharjee@gmail.com>
2021-08-08 00:55:37 +0530

    Server File Update

--- server/index.js
+++ server/index.js
@@ -1,27 +1,34 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const cors = require('cors');
-const dotenv = require('dotenv');
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const dotenv = require("dotenv");
 
-const postRoutes = require('./routes/posts.js');
+const postRoutes = require("./routes/posts.js");
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 dotenv.config();
 
-app.use(express.json()); //Express Built-in Body Parser
-app.use(express.urlencoded({
-    extended: true
-})); /**The urlencoded method within body-parser tells body-parser to extract data
-from the html element and add them to the body property in the request object. 
-We can access these using req.body*/
+app.use(express.json({limit: '50mb'}));
+app.use(
+  express.urlencoded({
+    limit: '50mb',
+    extended: true,
+  })
+); 
 app.use(cors());
 
-app.use('/posts', postRoutes); //All routes with suffix '/posts' is handled by postRoutes
+app.use("/posts", postRoutes); //All routes with suffix '/posts' is handled by postRoutes
+app.get('/', (req,res) => {
+  res.send('MemPic API: gitHub.com/toth2000/memPic');
+})
 
-mongoose.connect(process.env.MongoDb, 
-    {useNewUrlParser: true, useUnifiedTopology: true}) //Connecting to DB
-    .then(()=> app.listen(PORT, ()=> console.log(`Listening to PORT ${PORT}`)))
-    .catch((err)=> console.log('Error Connecting to DB' , err.message));
+mongoose
+  .connect(process.env.MongoDb, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  }) //Connecting to DB
+  .then(() => app.listen(PORT, () => console.log(`Listening to PORT ${PORT}`)))
+  .catch((err) => console.log("Error Connecting to DB", err.message));
 
-mongoose.set('useFindAndModify', false); //Setting variable and its value
+mongoose.set("useFindAndModify", false); //Setting variable and its value
--- server/routes/posts.js
+++ server/routes/posts.js
@@ -1,9 +1,18 @@
-const express = require('express');
-const {getPosts, createPost} = require('../controller/posts.js');
+const express = require("express");
+const {
+  getPosts,
+  createPost,
+  updatePost,
+  deletePost,
+  likePost,
+} = require("../controller/posts.js");
 
 const router = express.Router(); //Express Router
 
-router.get('/', getPosts);
-router.post('/', createPost);
+router.get("/", getPosts);
+router.post("/", createPost);
+router.patch("/:id", updatePost);
+router.delete("/:id", deletePost);
+router.patch("/:id/likePost", likePost);
 
-module.exports =  router;
+module.exports = router;
